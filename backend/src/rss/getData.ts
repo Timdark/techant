06cc4ef8c97ee09c72476ac7501f8acb0f73b834
Toni Pakarinen <toni.pakarinen@gmail.com>
2021-04-12 09:18:32 -0700

    Backend: anandtech rss get done

--- backend/src/rss/getData.ts
+++ backend/src/rss/getData.ts
@@ -4,11 +4,7 @@ import Parser from 'rss-parser';
 const prisma = new PrismaClient();
 let parser = new Parser();
 
-export default async () => {
-    // Clear table
-    await prisma.rssData.deleteMany({})
-
-    // Anandtech
+const anandtech = async () => {
     let feed = await parser.parseURL('https://www.anandtech.com/rss');
 
     let site = feed.title;
@@ -25,4 +21,28 @@ export default async () => {
             }
         })
     }
+}
+
+export default async () => {
+    // Clear table
+    await prisma.rssData.deleteMany({})
+
+    // Anandtech
+    /*let feed = await parser.parseURL('https://www.anandtech.com/rss');
+
+    let site = feed.title;
+    let icon = feed.image?.url;
+
+    for (const item of feed.items) {
+        await prisma.rssData.create({
+            data: {
+                siteName: String(site),
+                siteIcon: String(icon),
+                title: String(item.title),
+                linkUrl: String(item.link),
+                date: new Date(String(item.isoDate))
+            }
+        })
+    }*/
+    anandtech()
 };
